935d246468d1b8f6f2e86de35a908e01c0d72564
Mohak Bajaj <bmohak87@gmail.com>
2024-11-01 03:05:35 +0530

    Update title component styling

--- src/components/title.tsx
+++ src/components/title.tsx
@@ -1,6 +1,6 @@
 export default function Title({ name }: { name: string }) {
   return (
-    <h1 className="animate__animated animate__backInDown text-9xl font-bold text-primary">
+    <h1 className="animate__animated animate__backInDown text-center text-9xl font-bold text-primary">
       {name}
     </h1>
   );
