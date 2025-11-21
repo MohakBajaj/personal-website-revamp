cc2e89b309141583ec99db82d7fababf321d083c
Mohak Bajaj <bmohak87@gmail.com>
2025-11-21 19:50:26 +0530

    Update TypewriterText component to accept string arrays as input and change bun.lockb permissions

--- src/components/type-writer-text.tsx
+++ src/components/type-writer-text.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 
 interface TypewriterTextProps {
   typingSpeed?: number;
-  texts: TypeWriterTexts;
+  texts: TypeWriterTexts | string[];
 }
 
 export default function TypewriterText({
